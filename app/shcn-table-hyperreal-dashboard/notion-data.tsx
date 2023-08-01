8dcd71c5df2b3808f13af6e01191d8876d63f743
owolfdev <owolfdev@gmail.com>
2023-08-02 05:36:02 +0700

    progress on ui

---
+++ app/shcn-table-hyperreal-dashboard/notion-data.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {
+  getProcessedNotionData,
+  getNotionData,
+} from "@/utils/notion-data-utils";
+
+async function NotionData() {
+  const notionData = await getProcessedNotionData();
+  const notionData2 = await getNotionData();
+  // const transformedNotionData = transformNotionData(notionData);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        {notionData2?.map((item: any) => (
+          <div key={item.id}>
+            <div>Key tester: {item.name}</div>
+            <div>Key tester: {JSON.stringify(item.notes)}</div>
+
+            <div className="flex flex-col">
+              {Object.keys(item).map((key) => (
+                <div key={item.id}>{key}</div>
+              ))}
+            </div>
+            <div>{JSON.stringify(item)}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default NotionData;
